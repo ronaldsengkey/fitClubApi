c0910c283a0f51975e823c1e1a2511357001b557
Jimmy <jimmykonatra@gmail.com>
2019-12-12 23:18:00 +0700

    fixing a bug on register

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 const con = mysql.createPool({
-	host:process.env.MYSQL_HOST,
-	user:process.env.MYSQL_USER,
-	password:process.env.MYSQL_PASSWORD,
-	port:process.env.MYSQL_PORT,
-	database:process.env.MYSQL_DATABASE,
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	port: process.env.MYSQL_PORT,
+	database: process.env.MYSQL_DATABASE,
 	timezone: 'utc'
 });
-con.getConnection(function(err) {
-	if(!err){
+con.getConnection(function (err) {
+	if (!err) {
 		console.log('DB connect');
-	}else{
+	} else {
 		console.log("can't connect DB");
 		console.log(err);
 	}
