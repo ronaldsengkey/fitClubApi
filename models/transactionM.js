6f9ce708c23e068e806229640c46d96b3669d1fd
Ronald <RonaldSengkey@usahakreatif.co.id>
2020-01-27 10:59:17 +0700

    update param paymentRequest

--- models/transactionM.js
+++ models/transactionM.js
@@ -1,17 +1,59 @@
 const con = require('../config/dbConfig');
 let query = '',
-    message = '';
+    message = {};
 
-async function generateOtp() {
+async function generateNumber() {
     return Math.floor(10 + Math.random() * 90);
 }
 
+function tempTransaction(data){
+    return new Promise(async function (resolve, reject) {
+        let par = {
+            "userId": parseInt(data.userId),
+            "nominal": data.price,
+            "memberCat": data.memberCat,
+            "placeId": data.placeId,
+            "status": "0", //status 0 = pending, 1 = sukses , -1 = gagal
+            "paymentVia": data.paymentType, // cash / transfer
+            "requestCategory": data.requestCat
+        }
+        if(data.bankId){
+            par.bankId = data.bankId;
+        }
+        const query = "INSERT INTO temppayment SET ?";
+        con.query(query, par, async function (err, result) {
+            if (err) {
+                message = {
+                    "responseCode": process.env.ERRORINTERNAL_RESPONSE,
+                    "responseMessage": process.env.INTERNALERROR_MESSAGE
+                };
+                resolve(message);
+            } else { 
+                if (result.affectedRows > 0) {
+                    par.oldMemberCat = data.oldMemberCat;
+                    message = {
+                        "responseCode": process.env.SUCCESS_RESPONSE,
+                        "responseMessage": process.env.SUCCESS_MESSAGE,
+                        "data": par
+                    }
+                    resolve(message);
+                }else{
+                    message = {
+                        "responseCode": process.env.ERRORINTERNAL_RESPONSE,
+                        "responseMessage": process.env.INTERNALERROR_MESSAGE
+                    };
+                    resolve(message);
+                }
+            }
+        })
+    });
+}
 exports.transactionRequest = function (data) {
     return new Promise(async function (resolve, reject) {
         let query = "SELECT * FROM `memberfee` WHERE membercat = ? AND placeId = ?";
-        let a = await generateOtp();
+        let a = await generateNumber();
         let memberCat =JSON.parse(data.param);
-        await con.query(query,[memberCat.memberCategory, memberCat.placeId], (err, result) => {
+        await con.query(query,[memberCat.memberCategory, memberCat.placeId], async function (err, result)  {
             if (err) {
                 console.log("error get data", err)
                 message = {
@@ -21,12 +63,12 @@ exports.transactionRequest = function (data) {
                 resolve(message);
             } else { 
                 if (result.length > 0) {
-                    message = {
-                        "responseCode": process.env.SUCCESS_RESPONSE,
-                        "responseMessage": process.env.SUCCESS_MESSAGE,
-                        "data":{"price":result[0].fee+a}
-                    };
-                    resolve(message);
+                    let content = {"memberCat":memberCat.memberCategory,"price":parseInt(result[0].fee+a),"placeId":memberCat.placeId,"userId":data.profile.id,"bankId":memberCat.bankId, "paymentType":memberCat.paymentType, "requestCat":memberCat.requestCat};
+                    if(memberCat.oldMemberCat){
+                        content.oldMemberCat = memberCat.oldMemberCat;
+                    }
+                    let t = await tempTransaction (content);
+                    resolve(t);
                 } else {
                     message = {
                         "responseCode": process.env.NOTFOUND_RESPONSE,
