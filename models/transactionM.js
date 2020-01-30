980ed5d14ff98497a92ab93ebe80701bca7503a7
Jimmy <jimmykonatra@usahakreatif.co.id>
2020-01-30 10:14:42 +0700

    list transaction request

--- models/transactionM.js
+++ models/transactionM.js
@@ -1,12 +1,13 @@
 const con = require('../config/dbConfig');
 let query = '',
     message = {};
+var fs = require('fs');
 
 async function generateNumber() {
     return Math.floor(10 + Math.random() * 90);
 }
 
-function tempTransaction(data){
+function tempTransaction(data) {
     return new Promise(async function (resolve, reject) {
         let par = {
             "userId": parseInt(data.userId),
@@ -17,7 +18,7 @@ function tempTransaction(data){
             "paymentVia": data.paymentType, // cash / transfer
             "requestCategory": data.requestCat
         }
-        if(data.bankId){
+        if (data.bankId) {
             par.bankId = data.bankId;
         }
         const query = "INSERT INTO temppayment SET ?";
@@ -28,7 +29,7 @@ function tempTransaction(data){
                     "responseMessage": process.env.INTERNALERROR_MESSAGE
                 };
                 resolve(message);
-            } else { 
+            } else {
                 if (result.affectedRows > 0) {
                     par.oldMemberCat = data.oldMemberCat;
                     message = {
@@ -37,7 +38,7 @@ function tempTransaction(data){
                         "data": par
                     }
                     resolve(message);
-                }else{
+                } else {
                     message = {
                         "responseCode": process.env.ERRORINTERNAL_RESPONSE,
                         "responseMessage": process.env.INTERNALERROR_MESSAGE
@@ -52,8 +53,8 @@ exports.transactionRequest = function (data) {
     return new Promise(async function (resolve, reject) {
         let query = "SELECT * FROM `memberfee` WHERE membercat = ? AND placeId = ?";
         let a = await generateNumber();
-        let memberCat =JSON.parse(data.param);
-        await con.query(query,[memberCat.memberCategory, memberCat.placeId], async function (err, result)  {
+        let memberCat = JSON.parse(data.param);
+        await con.query(query, [memberCat.memberCategory, memberCat.placeId], async function (err, result) {
             if (err) {
                 console.log("error get data", err)
                 message = {
@@ -61,13 +62,21 @@ exports.transactionRequest = function (data) {
                     "responseMessage": process.env.INTERNALERROR_MESSAGE
                 };
                 resolve(message);
-            } else { 
+            } else {
                 if (result.length > 0) {
-                    let content = {"memberCat":memberCat.memberCategory,"price":parseInt(result[0].fee+a),"placeId":memberCat.placeId,"userId":data.profile.id,"bankId":memberCat.bankId, "paymentType":memberCat.paymentType, "requestCat":memberCat.requestCat};
-                    if(memberCat.oldMemberCat){
+                    let content = {
+                        "memberCat": memberCat.memberCategory,
+                        "price": parseInt(result[0].fee + a),
+                        "placeId": memberCat.placeId,
+                        "userId": data.profile.id,
+                        "bankId": memberCat.bankId,
+                        "paymentType": memberCat.paymentType,
+                        "requestCat": memberCat.requestCat
+                    };
+                    if (memberCat.oldMemberCat) {
                         content.oldMemberCat = memberCat.oldMemberCat;
                     }
-                    let t = await tempTransaction (content);
+                    let t = await tempTransaction(content);
                     resolve(t);
                 } else {
                     message = {
@@ -172,7 +181,7 @@ exports.getBank = function (data) {
                     }
                 })
             } else {
-                const query = "SELECT * FROM bank WHERE name = ?";
+                const query = "SELECT * FROM bank WHERE name = LIKE '%?%'";
                 con.query(query, [data.param], (err, res) => {
                     if (!err) {
                         if (err) {
@@ -213,4 +222,185 @@ exports.getBank = function (data) {
 async function generateNumber() {
     // return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
     return Math.random() * 100;
+}
+
+function checkUpload(data) {
+    return new Promise(async function (resolve, reject) {
+        let res = {};
+        try {
+            let query = "SELECT * FROM file WHERE transactionId = ?";
+            con.query(query, [data.transactionId], (err, res) => {
+                if (err) {
+                    console.log('Error query check upload data => ', err)
+                    message = {
+                        "responseCode": process.env.ERRORINTERNAL_RESPONSE,
+                        "responseMessage": "Internal server error"
+                    }
+                    resolve(message);
+                } else {
+                    if (res.length > 0) {
+                        message = {
+                            "responseCode": process.env.NOTACCEPT_RESPONSE,
+                            "responseMessage": "You have uploaded"
+                        };
+                        resolve(message);
+                    } else {
+                        message = {
+                            "responseCode": process.env.NOTFOUND_RESPONSE,
+                            "responseMessage": "No data uploaded"
+                        }
+                        resolve(message);
+                    }
+                }
+            })
+        } catch (e) {
+            console.log('Error checking upload data files => ', e);
+            res.responseCode = process.env.ERRORINTERNAL_RESPONSE;
+            res.responseMessage = "Internal server error";
+            resolve(res);
+        }
+    })
+}
+
+exports.uploadFiles = function (data) {
+    return new Promise(async function (resolve, reject) {
+        try {
+            console.log('data upload files => ', data);
+            let check = await checkUpload(data);
+            if (check.responseCode == process.env.NOTFOUND_RESPONSE) {
+                let upload = await uploadToLocal(data);
+                console.log('result upload => ', upload)
+                if (upload.responseCode == process.env.SUCCESS_RESPONSE) {
+                    let save = await saveFile(data);
+                    resolve(save);
+                } else {
+                    resolve(upload)
+                }
+            } else {
+                resolve(check);
+            }
+        } catch (error) {
+            console.log('Error uploading files => ', error)
+            message = {
+                "responseCode": process.env.ERRORINTERNAL_RESPONSE,
+                "responseMessage": process.env.INTERNALERROR_MESSAGE,
+            }
+            resolve(message)
+        }
+    })
+}
+
+function uploadToLocal(data) {
+    return new Promise(async function (resolve, reject) {
+        try {
+            let base64Image = data.file.split(';base64,').pop();
+            let name = new Buffer.from(data.id + ":" + new Date().toISOString()).toString('base64');
+            const type = data.file.split(';')[0].split('/')[1];
+            let path = `uploads/${name}.` + type;
+            fs.writeFile(path, base64Image, {
+                encoding: 'base64'
+            }, function (err) {
+                if (err) {
+                    console.log('file failed upload ', err);
+                    message = {
+                        "responseCode": process.env.ERRORINTERNAL_RESPONSE,
+                        "responseMessage": process.env.INTERNALERROR_MESSAGE,
+                    }
+                } else {
+                    message = {
+                        "responseCode": process.env.SUCCESS_RESPONSE,
+                        "responseMessage": process.env.SUCCESS_MESSAGE,
+                        "data": path
+                    }
+                }
+                resolve(message);
+            });
+        } catch (error) {
+            console.log('Error upload file => ', error)
+            message = {
+                "responseCode": process.env.ERRORINTERNAL_RESPONSE,
+                "responseMessage": process.env.INTERNALERROR_MESSAGE,
+            }
+            resolve(message)
+        }
+    })
+}
+
+function saveFile(data) {
+    return new Promise(async function (resolve, reject) {
+        let res = {};
+        try {
+            console.log('IN ', data)
+            query = "INSERT INTO file SET ?";
+            param = {
+                "file": data.file,
+                "category": "payment",
+                "transactionId": data.transactionId
+            }
+            con.query(query, param, (err, result) => {
+                console.log('result => ', result)
+                if (!err) {
+                    if (result.affectedRows > 0) {
+                        res.responseCode = process.env.SUCCESS_RESPONSE;
+                        res.responseMessage = "Success upload file";
+                    } else {
+                        res.responseCode = process.env.ERRORINTERNAL_RESPONSE;
+                        res.responseMessage = "Failed upload file";
+                    }
+                    console.log('res => ', res)
+                    resolve(res);
+                } else {
+                    console.log('Something error when save file => ', err)
+                    res.responseCode = process.env.ERRORINTERNAL_RESPONSE;
+                    res.responseMessage = "Failed upload file";
+                    resolve(res);
+                }
+            })
+        } catch (err) {
+            console.log('Something internal server error => ', err)
+            res.responseCode = process.env.ERRORINTERNAL_RESPONSE;
+            res.responseMessage = "Internal Server Error!";
+            resolve(res);
+        }
+    })
+}
+
+exports.listTransactionRequest = function (data) {
+    return new Promise(async function (resolve, reject) {
+        try {
+            let query = "SELECT * FROM temppayment";
+            con.query(query, (err, res) => {
+                if (err) {
+                    console.log('Error query get list transaction request => ', err)
+                    message = {
+                        "responseCode": process.env.ERRORINTERNAL_RESPONSE,
+                        "responseMessage": "Internal server error"
+                    }
+                    resolve(message);
+                } else {
+                    if (res.length > 0) {
+                        message = {
+                            "responseCode": process.env.SUCCESS_MESSAGE,
+                            "responseMessage": "List data transaction request",
+                            "data": res
+                        };
+                        resolve(message);
+                    } else {
+                        message = {
+                            "responseCode": process.env.NOTFOUND_RESPONSE,
+                            "responseMessage": "No data found"
+                        }
+                        resolve(message);
+                    }
+                }
+            })
+        } catch (error) {
+            console.log('Error get list transaction request => ', error)
+            message = {
+                "responseCode": process.env.ERRORINTERNAL_RESPONSE,
+                "responseMessage": process.env.INTERNALERROR_MESSAGE,
+            }
+            resolve(message)
+        }
+    })
 }
